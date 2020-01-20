4e126aca95612439f0b792048ca1c90e3068938c
Mattias Liloia <mattias.liloia@emilfrey.ch>
2020-01-20 08:30:42 +0100

    * remove unnessesary jpgs * separate dev and prod

--- src/config.js
+++ src/config.js
@@ -1,1 +1,26 @@
-export const BASE_URL = 'http://localhost:3001/';
+export const getContext = () => {
+  var context;
+  // GetGlobalContext defined by including reference to
+  // ClientGlobalContext.js.aspx in the HTML page.
+  if (typeof GetGlobalContext != "undefined") {
+    // eslint-disable-next-line no-undef
+    context = GetGlobalContext();
+  } else {
+    if (typeof Xrm != "undefined") {
+      // Xrm.Page.context defined within the Xrm.Page object model for form scripts.
+      // eslint-disable-next-line no-undef
+      context = Xrm.Page.context;
+    } else {
+      throw new Error("Context is not available.");
+    }
+  }
+  return context;
+};
+
+export const getClientUrl = () => {
+  if (process.env.NODE_ENV === "production") {
+    return getContext().getClientUrl() + "/api/data/v9.1";
+  } else {
+    return "http://localhost:3001/";
+  }
+};
