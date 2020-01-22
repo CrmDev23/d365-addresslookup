482a34b543dac3bb4ffcd3d0965a44ced54bb918
Mattias Liloia <mattias.liloia@emilfrey.ch>
2020-01-22 15:11:01 +0100

    * remove local state

--- src/components/OrtPickerSelect.js
+++ src/components/OrtPickerSelect.js
@@ -5,16 +5,7 @@ import { getPlzsByIds } from "../redux/selectors";
 import { ComboBox } from "office-ui-fabric-react/lib/ComboBox";
 
 class OrtPickerSelect extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      options: [],
-      initialDisplayValue: "Gränichen"
-    };
-  }
-
   render() {
-    const state = this.state;
     const { value, plzs } = this.props;
     let optionsKeyValue = plzs.map(plz => {
       return {
@@ -35,42 +26,15 @@ class OrtPickerSelect extends Component {
           onChange={(event, option, index, value) =>
             this.onChange(event, option, index, value)
           }
-          //onResolveOptions={currentOptions => this.getOptions(currentOptions)}
-          //text={optionsKeyValue[0].mat_plz_ortbez27}
         />
       </span>
     );
-  }
-
-  getOptions(currentOptions) {
-    if (this.state.options.length > 0) {
-      return this.state.options;
-    }
-
-    const { plzs, value } = this.props;
-    let optionsKeyValue = plzs.map(plz => {
-      return {
-        key: plz.mat_plzid,
-        text: plz.mat_plz_ortbez27
-      };
-    });
-
-    this.setState({
-      options: optionsKeyValue,
-      initialDisplayValue: undefined
-    });
-
-    return optionsKeyValue;
   }
 
   onChange(event, option, index, value) {
     const { setPlz } = this.props;
     console.log("_onChanged() is called: option = " + JSON.stringify(option));
     if (option) {
-      // User chose an existing option
-      //this.setState({
-      //  selectedOptionKey: option.key
-      //});
       setPlz(option.key);
     }
   }
