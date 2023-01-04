bb4460aa193e5cbcf3f52903a5427bda081d1741
Mattias Liloia <mattias.liloia@elca.ch>
2023-01-04 15:46:43 +0100

    * configure postman

--- src/config.js
+++ src/config.js
@@ -4,9 +4,13 @@ import store from './redux/store';
 export const NrOfImportChunks = 5;
 
 export const getClientUrl = () => {
-  const state = store.getState();
-  const crm_client_url = state.ui.parameters.crm_client_url;
-  return crm_client_url;
+  if (process.env.NODE_ENV === "production") {
+    const state = store.getState();
+    const crm_client_url = state.ui.parameters.crm_client_url;
+    return crm_client_url;
+  } else {
+    return "https://557e1598-f83c-4560-9aec-4d51d8eacd50.mock.pstmn.io/api/data/v9.1/";
+  }
 };
 
 export const getUserLocal = () => {
